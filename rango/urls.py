da7c72fff6ee0507577d9cc51517224af2340059
Katie Glynn <katie.glynn@hotmail.co.uk>
2017-02-16 16:13:22 +0000

    Update urls.py

--- rango/urls.py
+++ rango/urls.py
@@ -5,7 +5,7 @@ from rango import views
 
 urlpatterns = [
         url(r'^$', views.index, name='index'),
-        url(r'^about/', views.about, name='About'),
+        url(r'^about/', views.about, name='about'),
 
 ] + static(settings.MEDIA_URL,
        document_root=settings.MEDIA_ROOT)
